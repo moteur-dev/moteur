0b9c4b948dd0c1162a4539ec100439c2bdcaa772
Moteur Dev <dev@moteur.io>
2025-06-17 07:03:35 -0400

    feat: loginUser now returns an array with both the token and the user.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import loginRoute from './login';
+import loginRoute from './auth/login';
 import publicBlocks from './public/blocks';
 import publicFields from './public/fields';
 import adminBlocks from './admin/blocks';
@@ -8,49 +8,26 @@ import adminStructures from './admin/structures';
 import adminProjects from './admin/projects';
 import preview from './public/preview';
 import { moteurConfig } from '../../moteur.config';
-import { openApiSpec } from './openapi';
+import { loginUser } from '../api/auth';
 
 const app = express();
 app.use(express.json());
 
 // Base API path from config
 const basePath = moteurConfig.api.basePath ?? '/api/moteur';
+console.log(`Using base path: ${basePath}`);
 
-// Request token
-app.use('/login', loginRoute);
+app.use(basePath + '/auth', loginRoute);
 
 // Admin routes
-app.use(basePath + '/admin/blocks', adminBlocks);
-app.use(basePath + '/admin/fields', adminFields);
-app.use(basePath + '/admin/projects', adminProjects);
-app.use(basePath + '/admin/:project/structures', adminStructures);
+app.use(basePath + '/blocks', adminBlocks);
+app.use(basePath + '/fields', adminFields);
+app.use(basePath + '/projects', adminProjects);
+app.use(basePath + '/:project/structures', adminStructures);
 
-// Public routes
-app.use(basePath + '/blocks', publicBlocks);
-app.use(basePath + '/fields', publicFields);
 app.use(basePath + '/preview', preview);
-
-const router = express.Router();
-router.get('/openapi.json', (req, res) => {
-    res.json(openApiSpec);
-});
 
 const PORT = moteurConfig.api.port || process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Moteur API running at http://localhost:${PORT}`);
-    const routes: { methods: string[]; path: string }[] = [];
-
-    const parseRoute = (def: any) => {
-        if (def.route) {
-            routes.push({ path: def.route.path, methods: Object.keys(def.route.methods) });
-        } else if (def.name === 'router') {
-            // nested route (sub router)..
-            def.handle.stack.forEach(parseRoute);
-        }
-    };
-
-    // loop over and parse routes
-    app._router.stack.forEach(parseRoute);
-
-    console.log(routes);
 });
