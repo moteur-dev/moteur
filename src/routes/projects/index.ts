90ce736ba69f9f29ca8ec2004f9352cecfab568f
Moteur Dev <dev@moteur.io>
2025-06-20 10:19:11 -0400

    fix: Attach update/delete routes to router.

--- src/routes/projects/index.ts
+++ src/routes/projects/index.ts
@@ -3,16 +3,16 @@ import { Router } from 'express';
 import getAll, { openapi as getAllSpec } from './getAll';
 import getOne, { openapi as getOneSpec } from './getOne';
 import create, { openapi as createSpec, schemas as createSchemas, schemas } from './create';
-//import update from './update'
-//import remove from './delete'
+import update, { openapi as updateSpec } from './update';
+import remove, { openapi as deleteSpec } from './delete';
 
 const router = Router();
 
 router.use(getAll);
 router.use(getOne);
 router.use(create);
-//router.use(update)
-//router.use(remove)
+router.use(update);
+router.use(remove);
 
 export const projectsSpecs = {
     paths: {
