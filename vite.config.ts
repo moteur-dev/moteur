ef2628f7f82c8985c2014704d8fea9cda1b45047
Moteur Dev <dev@moteur.io>
2025-06-21 02:06:31 -0400

    Fix __dirname for ESM in vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
-import path from 'path';
+import path, {dirname} from 'path';
+import { fileURLToPath } from 'url';
+
+// ESM-safe __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   build: {
