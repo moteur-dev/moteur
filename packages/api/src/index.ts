47418a029fdfac885c6f3dd38eb5618b2073b234
moteur-dev <dev@moteur.io>
2025-07-08 09:24:07 -0400

    Add PATCH method to allowed by CORS
    Signed-off-by: moteur-dev <dev@moteur.io>

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(
     cors({
         origin: '*',
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization']
     })
 );
